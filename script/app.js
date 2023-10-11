d2e84be58959c7740db6ee8e28183877b00eab2c
Bas Van Massenhove <basvanmas@gmail.com>
2023-10-11 09:18:17 +0200

    Update css and add joke endpoint

--- script/app.js
+++ script/app.js
@@ -1,5 +1,29 @@
+let newJokeBtn, jokeElement;
+
 const init = function () {
-    console.log('script loaded');
-}
+  console.log("script loaded");
+  newJokeBtn = document.querySelector("#new-btn-joke");
+  jokeElement = document.querySelector("#joke");
+
+  newJokeBtn.addEventListener("click", getJoke);
+};
+
+const getJoke = function () {
+  console.log("getJoke()");
+  fetch("https://icanhazdadjoke.com/slack")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      const jokeText = data.attachments[0].text;
+      const jokeFallback = data.attachments[0].fallback;
+
+      if (jokeText !== "" && jokeText !== undefined) {
+        jokeElement.innerHTML = jokeText;
+      } else {
+        jokeElement.innerHTML = jokeFallback;
+      }
+    });
+};
 
 init();
